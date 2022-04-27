c2d91808566928a5dd35a5f06268bf1950c9e0a7
Thiago Fonseca de Souza <t.fs.2007@hotmail.com>
2022-04-26 23:18:16 -0300

    requisito 6

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,39 +2,62 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import Categories from './Categories';
 import Cards from '../components/Cards';
-import { getProductsFromCategoryAndQuery } from '../services/api';
+import { getProductsFromCategoryAndQuery, getCategories } from '../services/api';
 
 export default class Home extends Component {
   constructor() {
     super();
     this.state = {
-      categorie: '',
+      nameProduct: '',
       products: [],
+      categories: [],
+      nameCategory: '',
+      idCategory: '',
     };
+  }
+
+  getCategoriesApi = async () => {
+    const categories = await getCategories();
+    this.setState({
+      categories,
+    });
+  }
+
+  clickButton = ({ target }) => {
+    // console.log(event.target);
+    this.setState(() => ({
+      nameCategory: target.innerHTML,
+    }), async () => {
+      const { nameCategory, categories } = this.state;
+      const idCategory = categories.find((element) => element.name === nameCategory);
+      const getProducts = await getProductsFromCategoryAndQuery(idCategory.id, '');
+      this.setState({
+        products: getProducts.results,
+      });
+    });
   }
 
   handleChange = ({ target: { value } }) => {
     this.setState({
-      categorie: value,
+      nameProduct: value,
     });
   }
 
   handleClick = async () => {
-    const { categorie } = this.state;
-    const getProducts = await getProductsFromCategoryAndQuery('', categorie);
-    // console.log(getProducts.results);
+    const { nameProduct, idCategory } = this.state;
+    const getProducts = await getProductsFromCategoryAndQuery(idCategory, nameProduct);
     this.setState({
       products: getProducts.results,
     });
   }
 
   render() {
-    const { categorie, products } = this.state;
+    const { nameProduct, products, categories } = this.state;
     return (
       <div>
         <input
           type="text"
-          value={ categorie }
+          value={ nameProduct }
           data-testid="query-input"
           onChange={ this.handleChange }
         />
@@ -51,7 +74,11 @@ export default class Home extends Component {
           Digite algum termo de pesquisa ou escolha uma categoria.
         </p>
         <Link to="/shoppingCart" data-testid="shopping-cart-button"> Carrinho </Link>
-        <Categories />
+        <Categories
+          categories={ categories }
+          getCategoriesApi={ this.getCategoriesApi }
+          clickButton={ this.clickButton }
+        />
         {
           products.length === 0
             ? <p>Nenhum produto foi encontrado</p>
