76d437d31c6f6816d2d65cae4248d8f0082269ff
cafe51 <cafecafe51@hotmail.com>
2022-04-29 20:42:51 -0400

    refatorado

--- src/App.js
+++ src/App.js
@@ -3,14 +3,12 @@ import { Route, Switch, BrowserRouter } from 'react-router-dom';
 import Home from './pages/Home';
 import ShoppingCart from './pages/ShoppingCart';
 import ProductDetail from './pages/ProductDetail';
-import CartCounter2 from './components/CartCounter2';
 import './App.css';
 
 export default class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <CartCounter2 />
         {/* nota: tem 2 browserRouter. O outro está no index  */}
         <Switch>
           <Route
